058047309c8dd2fa56164156b4366a93318d82ad
Cajetan <cajemma122@gmail.com>
2025-10-16 14:12:22 +0100

    Add initial implementation of myFriend function

---
+++ file.js
@@ -0,0 +1,11 @@
+const myNumber = 2;
+ if ( for (i = 4; i< 5; i++){
+      const myFriend = (name) =>{
+      i = ? `thats is good` : `not good`';
+      return `my Friend ${name} is the best`;
+      }
+  myFriend(Esther);
+   
+ }){
+   consloe.log(myFriend(Esther))
+ }
